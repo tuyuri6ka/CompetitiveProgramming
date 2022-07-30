826dc6798ce3047188ef4b8c5abad915496463db
tuyuri6ka <m.motoike.kra@gmail.com>
2022-07-30 16:32:01 +0900

    Update: 3_4 安定なソートについてを追加完了した。

--- references/book/algorithm_and_data_structure/3_4_stable_sort.go
+++ references/book/algorithm_and_data_structure/3_4_stable_sort.go
@@ -19,18 +19,20 @@ func main() {
 	// 問題に適した形に整形する
 	processingInput := inputProcessing(input)
 
-	result := bubbleSort(processingInput)
+	result_by_bubble_sort := bubbleSort(processingInput)
+	output_result(true, result_by_bubble_sort)
 
-	fmt.Println(result)
-
+	result_by_selection_sort := selectionSort(processingInput)
+	isStable := isStable(result_by_bubble_sort, result_by_selection_sort)
+	output_result(isStable, result_by_selection_sort)
 }
 
 func inputSource(N int) []string {
 	input := make([]string, N, N)
-
 	for i := 0; i < N; i++ {
 		fmt.Scan(&input[i])
 	}
+
 	return input
 }
 
@@ -40,22 +42,72 @@ func inputProcessing(input []string) []Card {
 		result[i].suit = v[:1]
 		result[i].value, _ = strconv.Atoi(v[1:])
 	}
+
 	return result
 }
 
+// bubbleSortは常に安定なソート（入力の順番を変えない）
 func bubbleSort(card []Card) []Card {
-	result := card
+	// 引数を直接変更しないようにディープコピーする
+	result := make([]Card, len(card), cap(card))
+	copy(result, card)
+
 	for i := 0; i < len(result); i++ {
 		for j := len(result) - 1; j > i; j-- {
 			if result[j-1].value > result[j].value {
-				big := result[j-1].value
-				small := result[j].value
+				big := result[j-1]
+				small := result[j]
 
-				result[j-1].value = small
-				result[j].value = big
+				result[j-1] = small
+				result[j] = big
 			}
 		}
 	}
 
 	return result
+}
+
+// selectionSortは不安定なソート（入力の順番が変わりうる）
+func selectionSort(card []Card) []Card {
+	// 引数を直接変更しないようにディープコピーする
+	result := make([]Card, len(card), cap(card))
+	copy(result, card)
+
+	for i := 0; i < len(result); i++ {
+		minj := i
+		for j := i; j < len(result); j++ {
+			if result[minj].value > result[j].value {
+				minj = j
+			}
+		}
+		tmp := result[i]
+		result[i] = result[minj]
+		result[minj] = tmp
+	}
+
+	return result
+}
+
+func isStable(bubble_card []Card, selection_card []Card) bool {
+	var flag bool
+	for i, v := range bubble_card {
+		if v.suit == selection_card[i].suit && v.value == selection_card[i].value {
+			// 何もしない
+		} else {
+			flag = false
+		}
+	}
+
+	return flag
+}
+
+func output_result(is_ok bool, result []Card) bool {
+	if is_ok == true {
+		fmt.Println("Stable")
+	} else {
+		fmt.Println("Not stable")
+	}
+	fmt.Println(result)
+
+	return true
 }
