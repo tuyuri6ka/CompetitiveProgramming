0dbce07119b138c9b366db807e34baa738a62082
tuyuri6ka <m.motoike.kra@gmail.com>
2022-07-30 12:35:50 +0900

    Update: 3_5: メソッドを職責に分けた。

--- references/book/algorithm_and_data_structure/3_4_stable_sort.go
+++ references/book/algorithm_and_data_structure/3_4_stable_sort.go
@@ -6,22 +6,33 @@ import (
 )
 
 func main() {
+	// 標準入力を取得する
 	var N int
 	fmt.Scan(&N)
+	input := inputSource(N)
 
+	// 問題に適した形に整形する
+	result := inputProcessing(input)
+
+	fmt.Println(result)
+}
+
+func inputSource(N int) []string {
 	input := make([]string, N, N)
+
 	for i := 0; i < N; i++ {
 		fmt.Scan(&input[i])
 	}
+	return input
+}
 
-	fmt.Println(input)
-	// ハッシュのスライス
-	hash := make([]map[string]int, N, N)
+func inputProcessing(input []string) []map[string]int {
+	result := make([]map[string]int, len(input), cap(input))
 	for i, _ := range input {
-		tmp := make(map[string]int)
-		tmp[input[i][:1]], _ = strconv.Atoi(input[i][1:])
-		hash[i] = tmp
-	}
-	fmt.Println(hash)
+		hash := make(map[string]int)
+		hash[input[i][:1]], _ = strconv.Atoi(input[i][1:])
 
+		result[i] = hash
+	}
+	return result
 }
